364615da1f0468be73480db929d9d683d3dbe270
Nxdus <54182974+Nxdus@users.noreply.github.com>
2025-03-17 16:57:42 +0700

    update next.config.ts to enable basePath and assetPrefix for improved routing and image handling

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/about-me",
-  // assetPrefix: "/about-me/",
-  // images: {
-  //   domains: ["nxdus.github.io"],
-  // },
+  basePath: "/about-me",
+  assetPrefix: "/about-me/",
+  images: {
+    domains: ["nxdus.github.io"],
+  },
 };
 
 export default nextConfig;
